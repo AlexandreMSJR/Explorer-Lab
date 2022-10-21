57fffc0e5607b43f31afbfe4d44a32ec3252f186
Alexandre Medeiros <alexandremedeirosjunior@gmail.com>
2022-10-21 13:13:55 -0300

    Update project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./css/index.css"
+
+const ccBgColor1 = document.querySelector(
+  "#app > section > div.cc-bg > svg > g > g:nth-child(1) > path"
+)
+const ccBgColor2 = document.querySelector(
+  "#app > section > div.cc-bg > svg > g > g:nth-child(2) > path"
+)
+
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#2D57F2", "#436D99"],
+    masterCard: ["#DF6F29", "#C69347"],
+    default: ["black", "grey"],
+    cielo: ["#060215", "#C9101B"],
+  }
+
+  ccBgColor1.setAttribute("fill", colors[type][0])
+  ccBgColor2.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
